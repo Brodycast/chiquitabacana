88e60e937cb8cb8052f98cacfddad585b0ef509e
Henri Cavalcante <hnri_mxel@hotmail.com>
2016-08-19 00:13:08 -0300

    testing release

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main';
-
-var div = document.createElement("div");
-ReactDOM.render(<App />, div);
-var container = document.body;
-container.replaceChild(div.querySelector("#app"), document.getElementById("app"));
